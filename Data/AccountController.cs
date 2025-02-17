cad587371e4a419ef3d061baf51f4e43257f4c18
Sandip Mistry <SMistry@rensafamily.company>
2025-02-17 14:21:27 +0600

    add register

---
+++ Data/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using IdentityApp.Models;
+using IdentityApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Data
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Register(string returnUrl = null)
+        {
+            var viewModel = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl ?? Url.Content("~/")
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel , string returnUrl) 
+        {
+            returnUrl ??= Url.Content("~/");
+            viewModel.ReturnUrl = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = viewModel.UserName, Email = viewModel.Email };
+                var result = await _userManager.CreateAsync(user, viewModel.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(viewModel);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,14 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+builder.Services.AddIdentity<AppUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
